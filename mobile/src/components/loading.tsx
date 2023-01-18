73b5767fe5189321dec726ea29ea1949fe061cd7
Herllon <herllon@pop-os.localdomain>
2023-01-18 17:31:20 +0000

    kickoff

---
+++ mobile/src/components/loading.tsx
@@ -0,0 +1,8 @@
+import { View, ActivityIndicator } from 'react-native';
+export function Loading() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <ActivityIndicator color='#7C3AED' />
+    </View>
+  );
+}
